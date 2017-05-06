508de58f059eca708528b4ca5c25faa2462912f4
Emma Claire Philippides <acme9898@gmail.com>
2017-05-05 21:49:03 -0700

    changes to lib file and views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,14 +6,9 @@ class RecipesController < ApplicationController
 
   def search_result
     @recipes = Recipe.search_result(params[:search])
-    # puts "test"
-    # puts @recipes
-    if @recipes.nil?
-      head :not_found
-    end
   end
 
   def show
-    @recipe = Recipe.show_recipe(params[:uri])  #how to assign id?
+    @recipe = Recipe.show_recipe(params[:label])  #how to assign id?''
   end
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -6,13 +6,14 @@ class Recipe
 
   BASE_URL = "https://api.edamam.com/search?"
 
-  attr_reader :label, :image, :uri, :source, :ingredients, :nutrition, :uri
+  attr_reader :label, :image, :id, :source, :ingredients, :ingredientlines, :nutrition
 
   def initialize(recipe_params)
     @label = recipe_params[:label]
     @image = recipe_params[:image]
-    @uri = recipe_params[:uri]
+    @id = recipe_params[:id]
     @source = recipe_params[:source]
+    @ingredientlines = recipe_params[:ingredientlines]
     @ingredients = recipe_params[:ingredients]
     @nutrition = recipe_params[:nutrition]
   end
@@ -29,29 +30,40 @@ class Recipe
 
     recipe_array = []
     search_result.each do |recipe|
-      recipe_data = {label: recipe["recipe"]["label"], image: recipe["recipe"]["image"], uri: recipe["recipe"]["uri"]} #.gsub("#", "%23")
+      recipe_id_from_uri = recipe["recipe"]["uri"].split("_").last
+      recipe_data = {label: recipe["recipe"]["label"], image: recipe["recipe"]["image"], id: recipe_id_from_uri}  #ask someone about how to get id from uri using gsub
       recipe_array << Recipe.new(recipe_data)
+
     end
     return recipe_array
   end
 
-  def self.show_recipe(uri)
+  def self.show_recipe(recipe_id_from_uri)
     query_params = {
       "app_id" => ENV["EDAMAM_APPLICATION_ID"],
       "app_key" => ENV["EDAMAM_APPLICATION_KEY"],
-      "r" => uri
+      "q" => recipe_id_from_uri
     }
 
-    search_result = HTTParty.get(BASE_URL, query: query_params)
+    search_result = HTTParty.get(BASE_URL, query: query_params).parsed_response["hits"]
 
     show_recipe = {}
 
-  show_recipe[:label] = search_result[0][:label]
-  show_recipe[:image] = search_result[0][:image]
-  show_recipe[:ingredients] = search_result[0][:ingredientLines]
-  show_recipe[:source] = search_result[0][:source] #should I add url here?
-  #ask whether I can skip nutrients
+    show_recipe[:label] = search_result[0]["recipe"]["label"]
 
-return Recipe.new(recipe_info)
+    show_recipe[:image] = search_result[0]["recipe"]["image"]
+
+    show_recipe[:ingredients] = search_result[0]["recipe"]["ingredients"]
+
+    show_recipe[:ingredientlines] = search_result[0]["recipe"]["ingredientlines"]
+
+    show_recipe[:source] = search_result[0]["recipe"]["source"]
+
+    show_recipe[:nutrition] = search_result[0]["recipe"]["nutrition"]
+
+    show_recipe[:uri] = search_result[0]["recipe"]["id"] #should I add url here?
+    #ask whether I can skip nutrients
+
+    return Recipe.new(show_recipe)
   end
 end
