f08e293eef638661368d980a93bbe9640edb027a
Emma Claire Philippides <acme9898@gmail.com>
2017-05-06 19:29:15 -0700

    changes to views

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -6,7 +6,7 @@ class Recipe
 
   BASE_URL = "https://api.edamam.com/search?"
 
-  attr_reader :label, :image, :id, :source, :ingredients, :nutrition, :url; :yield
+  attr_reader :label, :image, :id, :source, :ingredients, :nutrition, :url, :yield, :calories
 
   def initialize(recipe_params)
     @label = recipe_params[:label]
@@ -17,6 +17,7 @@ class Recipe
     @ingredients = recipe_params[:ingredients]
     @yield = recipe_params[:yield]
     @nutrition = recipe_params[:nutrition]
+    @calories = recipe_params[:calories]
   end
 
   def self.search_result(ingredient) #or search_result?
@@ -32,7 +33,7 @@ class Recipe
     recipe_array = []
     search_result.each do |recipe|
       recipe_id_from_uri = recipe["recipe"]["uri"].split("_").last
-      recipe_data = {label: recipe["recipe"]["label"], image: recipe["recipe"]["image"], id: recipe_id_from_uri}  #ask someone about how to get id from uri using gsub
+      recipe_data = {label: recipe["recipe"]["label"], image: recipe["recipe"]["image"], source: recipe["recipe"]["source"], url: recipe["recipe"]["url"], calories: recipe["recipe"]["calories"], yield: recipe["recipe"]["yield"], id: recipe_id_from_uri, ingredients: recipe["recipe"]["ingredients"]}  #ask someone about how to get id from uri using gsub
       recipe_array << Recipe.new(recipe_data)
 
     end
@@ -57,11 +58,11 @@ class Recipe
     show_recipe[:ingredients] = search_result[0]["recipe"]["ingredientLines"]
 
     show_recipe[:yield]= search_result[0]["recipe"]["yield"]
-
     show_recipe[:source] = search_result[0]["recipe"]["source"]
     show_recipe[:url]= search_result[0]["recipe"]["url"]
 
     show_recipe[:nutrition] = search_result[0]["recipe"]["totalNutrients"]
+    show_recipe[:calories] = search_result[0]["calories"]
 
     show_recipe[:uri] = search_result[0]["recipe"]["id"] #should I add url here?
     #ask whether I can skip nutrients
