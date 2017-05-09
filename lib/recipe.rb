9da430328b563f9ff52df8dccf2da6a1ad30f75e
Emma Claire Philippides <acme9898@gmail.com>
2017-05-09 14:15:21 -0700

    Rails.logger

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -27,15 +27,15 @@ class Recipe
       "app_key" => ENV["EDAMAM_APPLICATION_KEY"],
       "q" => ingredient
     }
-    logger.debug "=================================="
-    logger.debug "query_params = #{query_params}"
-    logger.debug "=================================="
+    Rails.logger.debug "=================================="
+    Rails.logger.debug "query_params = #{query_params}"
+    Rails.logger.debug "=================================="
 
 
     search_result = HTTParty.get(BASE_URL, query: query_params).parsed_response["hits"] #add from to here
-    logger.debug "=================================="
-    logger.debug "search_result = #{search_result}"
-    logger.debug "=================================="
+    Rails.logger.debug "=================================="
+    Rails.logger.debug "search_result = #{search_result}"
+    Rails.logger.debug "=================================="
 
     recipe_array = []
     search_result.each do |recipe|
